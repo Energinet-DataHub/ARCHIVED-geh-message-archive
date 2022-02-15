585d83bf70fab6b2c1744ae65c88d8279240d9b5
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-15 13:15:54 +0100

    add cosmos to vnet

--- build/infrastructure/main/message-archive-cosmos.tf
+++ build/infrastructure/main/message-archive-cosmos.tf
@@ -11,45 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_cosmosdb_account" "message_archive" {
-  name                = "cosmos-logs-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-  # To enable global failover change to true and uncomment second geo_location
-  enable_automatic_failover = false
-
-  consistency_policy {
-    consistency_level = "Session"
-  }
-  
-  geo_location {
-    location          = azurerm_resource_group.this.location
-    failover_priority = 0
-  }
-
-  tags                = azurerm_resource_group.this.tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }
+module "message_archive" {
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/cosmos-db-account?ref=6.0.0"
+  name                                      = "logs"
+  project_name                              = var.domain_name_short
+  environment_short                         = var.environment_short
+  environment_instance                      = var.environment_instance
+  resource_group_name                       = azurerm_resource_group.this.name
+  location                                  = azurerm_resource_group.this.location
+  private_endpoint_subnet_id                = module.snet_internal_private_endpoints.id
+  private_dns_resource_group_name           = data.azurerm_key_vault_secret.pdns_resource_group_name.value
 }
 
 resource "azurerm_cosmosdb_sql_database" "db" {
   name                = "message-archive"
   resource_group_name = azurerm_resource_group.this.name
-  account_name        = azurerm_cosmosdb_account.message_archive.name
+  account_name        = module.message_archive.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "collection_catalog" {
   name                = "logs"
   resource_group_name = var.resource_group_name
-  account_name        = azurerm_cosmosdb_account.message_archive.name
+  account_name        = module.message_archive.name
   database_name       = azurerm_cosmosdb_sql_database.db.name
   partition_key_path  = "/partitionKey"
 }
