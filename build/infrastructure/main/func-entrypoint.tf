fb5a91f2e6735e09cee3d5b2c3b52779052a04b9
KarstenM <xnetsrak@gmail.com>
2022-03-10 15:17:53 +0100

    adding func base url to keyvault

--- build/infrastructure/main/func-entrypoint.tf
+++ build/infrastructure/main/func-entrypoint.tf
@@ -38,3 +38,13 @@ module "func_entrypoint_messagearchive" {
   
   tags                                      = azurerm_resource_group.this.tags
 }
+
+module "kvs_app_message_archive_api_base_url" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
+
+  name          = "app-message_archive-api-base-url"
+  value         = "https://${func_entrypoint_messagearchive.default_hostname}/api/"
+  key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
+
+  tags          = azurerm_resource_group.this.tags
+}
