35a49c1e847bda6ce3cebabf9f1d0d5eeb8dc20f
KarstenM <xnetsrak@gmail.com>
2022-04-08 11:28:04 +0200

    change test

--- source/Energinet.DataHub.MessageArchive.IntegrationTests/Repositories/ArchiveReaderRepositoryTests.cs
+++ source/Energinet.DataHub.MessageArchive.IntegrationTests/Repositories/ArchiveReaderRepositoryTests.cs
@@ -20,6 +20,7 @@ using Energinet.DataHub.MessageArchive.EntryPoint;
 using Energinet.DataHub.MessageArchive.Persistence.Containers;
 using Energinet.DataHub.MessageArchive.PersistenceModels;
 using Energinet.DataHub.MessageArchive.Reader;
+using Energinet.DataHub.MessageArchive.Reader.Handlers;
 using Energinet.DataHub.MessageArchive.Reader.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -162,7 +163,7 @@ namespace Energinet.DataHub.MessageArchive.IntegrationTests.Repositories
                 x => x.Container.Options.EnableAutoVerification = false);
             startup.Container.Options.AllowOverridingRegistrations = true;
             var scope = AsyncScopedLifestyle.BeginScope(startup.Container);
-            var archiveReaderRepository = scope.GetInstance<IArchiveReaderRepository>();
+            var archiveReaderRepository = scope.GetInstance<IArchiveSearchHandler>();
             var archiveContainer = scope.GetInstance<IArchiveContainer>();
 
             var testGroup = Guid.NewGuid().ToString();
@@ -200,11 +201,11 @@ namespace Energinet.DataHub.MessageArchive.IntegrationTests.Repositories
                 null);
 
             // Act
-            var result = await archiveReaderRepository.GetSearchResultsAsync(searchCriteria).ConfigureAwait(false);
+            var (searchResult, validationResult) = await archiveReaderRepository.SearchAsync(searchCriteria).ConfigureAwait(false);
 
             // Assert
-            var inDbResult = result.Result.FirstOrDefault(e => e.MessageId == messageIdIn);
-            var outDbResult = result.Result.FirstOrDefault(e => e.OriginalTransactionIDReferenceId == messageIdIn);
+            var inDbResult = searchResult.Result.FirstOrDefault(e => e.MessageId == messageIdIn);
+            var outDbResult = searchResult.Result.FirstOrDefault(e => e.OriginalTransactionIDReferenceId == messageIdIn);
 
             Assert.NotNull(inDbResult);
             Assert.NotNull(outDbResult);
