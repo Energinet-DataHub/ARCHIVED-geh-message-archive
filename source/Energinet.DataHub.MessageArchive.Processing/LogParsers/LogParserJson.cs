8a5040d18502657134e526326c36892123bc9e4a
KarstenM <xnetsrak@gmail.com>
2022-10-31 14:37:32 +0100

    updated usings and missing license

--- source/Energinet.DataHub.MessageArchive.Processing/LogParsers/LogParserJson.cs
+++ source/Energinet.DataHub.MessageArchive.Processing/LogParsers/LogParserJson.cs
@@ -14,7 +14,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Energinet.DataHub.MessageArchive.PersistenceModels;
